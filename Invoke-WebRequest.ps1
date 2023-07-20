656b200c4532867dff65e8e05b1ebb690eb8bfff
Gustavo Carvalho <gustavo.carvalho.br@gmail.com>
2023-07-20 09:10:32 -0400

    Update Invoke-WebRequest.ps1

--- Invoke-WebRequest.ps1
+++ Invoke-WebRequest.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://uvermont.mm.fcix.net/videolan-ftp/vlc/3.0.18/win32/vlc-3.0.18-win32.exe" -OutFile "C:\%username%"\Downloads"
+Invoke-WebRequest -Uri "https://putTheAdressHere.com/template.exe" -OutFile "C:\%username%"\Downloads"
