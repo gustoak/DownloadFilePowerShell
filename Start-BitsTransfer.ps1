87d30d3c77ccb69a54e8654c29d5155a5ddd8d3b
Gustavo Carvalho <gustavo.carvalho.br@gmail.com>
2023-07-20 09:11:25 -0400

    Update Start-BitsTransfer.ps1

--- Start-BitsTransfer.ps1
+++ Start-BitsTransfer.ps1
@@ -1,3 +1,3 @@
-$URL = "https://uvermont.mm.fcix.net/videolan-ftp/vlc/3.0.18/win32/vlc-3.0.18-win32.exe" 
+$URL = "https://putTheAddressHere.com/test.exe" 
 $Path = "C:\%username%"\Downloads"
 Start-BitsTransfer -Source $URL -Destination $Path
